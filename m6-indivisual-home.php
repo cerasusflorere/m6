49f5f42f580152e8108014b409a644ca4b7df660
cerasusflorere <86288675+cerasusflorere@users.noreply.github.com>
2021-07-10 22:41:13 +0900

    Update m6-indivisual-home.php

--- m6-indivisual-home.php
+++ m6-indivisual-home.php
@@ -6,7 +6,7 @@
 </head>
      <?php 
          //クロスサイトリクエストフォージェリ（CSRF）対策
-         　　$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+             $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
              $token = $_SESSION['token'];
          //クリックジャッキング対策
              header('X-FRAME-OPTIONS: SAMEORIGIN'); 
@@ -18,70 +18,60 @@
          // ・データベース名：***
          // ・ユーザー名：***
          // ・パスワード：***
-    
+
          // DB接続設定
          $dsn = 'mysql:dbname=***;host=***';
          $user = '***';
          $password = '***';
          $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
-         if(empty($_GET)){
-             header("Location:m6-login-form.php");
-             exit();
-         else{
-             $username = isset($_SESSION["username"]) ? $_SESSION["username"] : NULL;
+         $username = isset($_SESSION["username"]) ? $_SESSION["username"] : NULL;
+         define("table_name", $username);
              
-             $sql = "CREATE TABLE IF NOT EXISTS '{$username}'"
-             ." ("
-             . "id INT AUTO_INCREMENT PRIMARY KEY,"
-             . "username varchar(280),"
-             . "subject varchar(280)"
-             .");";
-             $stmt = $pdo->query($sql); 
-             
-             try{
-                 $sql_home = "SELECT * FROM '$username' ";
-                 $stmt_home = $pdo -> query($sql_home);
-                 $stmt_home -> execute();
-                 $results  = $stmt_home -> fetch();
-             
-                 if(is_array($results)){
-                     foreach($results as $row){
-                         $_SESSION['id'] = $row['id'];
-                         echo intval($row['id'])-1; 
-                         $link = '<a href = "m6-indivisual-subject-show.php">'.$row['subject'].'</a>';
-                         echo ": ".$link."<br>";
-                     }
-                 }else{
-                     echo "まだデータがありません。";
-                     $sql_first = $pdo -> prepare("INSERT INTO {'$username'} (username) VAULE (:username)");
-                     $sql_first -> bindValue(':username', $username, PDO::PARAM_STR);
-                     $sql_first -> execute();
-                 }
-             }catch(PDOException $e){
-                 //トランザクション取り消し
-                 $pdo -> rollBack();
-                 $errors['error'] = "もう一度やり直してください。";
-                 print('Error:'.$e->getMessage());
-             }
-         }
+         $sql = "CREATE TABLE IF NOT EXISTS table_name"
+         ." ("
+         . "id INT AUTO_INCREMENT PRIMARY KEY,"
+         . "username varchar(280),"
+         . "subject varchar(280)"
+         .");";
+         $stmt = $pdo->query($sql); 
          
-         /** 
-          * 追加する（btn_edit）を押した後の処理
-         */
-         if(isset($_POST['btn_edit'])){
-             if(empty($_GET)){
-                 header("Location:m6-login-form.php");
-                 exit();
+         try{
+             $sql_home = "SELECT * FROM table_name ";
+             $stmt_home = $pdo -> prepare($sql_home);
+             $stmt_home -> execute();
+             $results  = $stmt_home -> fetch();
+         
+             if(is_array($results)){
+                 foreach($results as $row){
+                     $_SESSION['id'] = $row['id'];
+                     echo intval($row['id'])-1; 
+                     $link = '<a href = "m6-indivisual-subject-show.php">'.$row['subject'].'</a>';
+                     echo ": ".$link."<br>";
+                 }
              }else{
-                 $_SESSION['id'] = NULL;
-                 header("Location:m6-indivisual-add.php");
-                 exit();
+                 echo "まだデータがありません。<br>";
              }
+         }catch(PDOException $e){
+             //トランザクション取り消し
+             $pdo -> rollBack();
+             $errors['error'] = "もう一度やり直してください。";
+             print('Error:'.$e->getMessage());
+         }
+     
+         /** 
+          * 追加する（btn_add）を押した後の処理
+         */
+         if(isset($_POST['btn_add'])){
+             unset($_SESSION['id']);
+             header("Location:m6-indivisual-subject-add.php");
+             exit();
          }
      ?>
      
-     <input type="submit" name="btn_edit" value="追加する"><br>
-     <p><a href = "m6-logout.php">ログアウトはこちら</a></p>
-     <p><a href = "m6-withdrow.php">退会する</a></p>
-     </html>  
+     <form action="" method="post">     
+         <input type="submit" name="btn_add" value="追加する">
+         <p><a href = "m6-logout.php">ログアウトはこちら</a></p>
+         <p><a href = "m6-withdrow.php">退会する</a></p>
+     </form>
+</html>  
