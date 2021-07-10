90aa52eef89cd33f34d2a9b01818573c66b0570b
cerasusflorere <86288675+cerasusflorere@users.noreply.github.com>
2021-07-10 13:26:16 +0900

    Update m6-create.php

--- m6-create.php
+++ m6-create.php
@@ -6,6 +6,7 @@
 </head>
 
     <?php
+         session_start();
          //クロスサイトリクエストフォージェリ（CSRF）対策
              $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
              $token = $_SESSION['token'];
@@ -19,7 +20,7 @@
          // ・データベース名：***
          // ・ユーザー名：***
          // ・パスワード：***
-
+	
          // DB接続設定
          $dsn = 'mysql:dbname=***;host=***';
          $user = '***';
@@ -39,18 +40,29 @@
 	         if ($urltoken === ''){
 		        $errors['urltoken'] = "トークンがありません。";
 	         }else{
-	            $nowdate = date('YYYY-MM-DD HH:MM:SS');
+	            $flag = 0;
+	            $date = date('Y-m-d H:i:s',strtotime("- 24hours"));
 	            //flagが0の未登録者 or 仮登録日から24時間以内
-			    $sql_confirm = "SELECT email FROM pre_user WHERE urltoken=(:urltoken) AND flag = 0 AND date > now() - interval 24 hour";
-                            $stmt_confirm = $pdo->prepare($sql_confirm);
+			    $sql_confirm = "SELECT email FROM pre_user WHERE urltoken=:urltoken AND (flag=:flag OR date<=:date)";
+                $stmt_confirm = $pdo->prepare($sql_confirm);
 			    $stmt_confirm->bindValue(':urltoken', $urltoken, PDO::PARAM_STR);
+			    $stmt_confirm->bindValue(':flag', $flag, PDO::PARAM_INT);
+			    $stmt_confirm->bindValue(':date', $date, PDO::PARAM_STR);
 			    $stmt_confirm->execute();
 			    $count_confirm = $stmt_confirm -> rowCount();
 			 
 		    	//24時間以内に仮登録され、本登録されていないトークンの場合
-			    if($count_confirm ==　1){
-				     $email_array = $stmt_confirm -> fetch();
-				     $email = $email_array["email"];
+			    if($count_confirm == 1){
+			         $sql_confirm = "SELECT * FROM pre_user WHERE urltoken=:urltoken AND (flag=:flag OR date<=:date)";
+			         $stmt_confirm = $pdo -> prepare($sql_confirm);
+			         $stmt_confirm -> bindValue(':urltoken', $urltoken, PDO::PARAM_STR);
+			         $stmt_confirm -> bindValue(':flag', $flag, PDO::PARAM_INT);
+			         $stmt_confirm -> bindValue(':date', $date, PDO::PARAM_STR);
+			         $stmt_confirm -> execute();
+				     $email_array = $stmt_confirm -> fetchAll();
+				     foreach($email_array as $row){
+				          $email = $row['email'];
+				     }
 				     $_SESSION['email'] = $email;
 			    }else{
 				     $errors['urltoken_timeover'] = "このURLはご利用できません。有効期限が過ぎたかURLが間違えている可能性がございます。もう一度登録をやりなおして下さい。";
@@ -69,31 +81,50 @@
                  exit();
              }else{
                  //POSTされたデータを変数に入れる
-                 $name = isset($_POST['username']) ? $_POST['username']:NULL;
+                 $username = isset($_POST['username']) ? $_POST['username']:NULL;
                  $password = isset($_POST['password']) ? $_POST['password']:NULL;
-                 $password_confirm = isset($_POST['password_confirm']) ? $_POST['password_confirm']:NULL;
+                 $confirm_password = isset($_POST['confirm_password']) ? $_POST['confirm_password']:NULL;
                  
                  //セッションに登録
-                 $_SESSION['username'] = $name;
-                 $_SESSION['password'] = $password;
+                 
+                 
                  
                  //アカウント入力判定
                  //パスワード入力判定
-                 if($password != $password_confirm){
-                     $errors['password_confirm'] = "パスワードが一致しません。";
-                 }
-                 
-                 if(count($errors) == 0 && ($password === "" || $username === "")){
-                     if($password === ""){
-                         $errors['password'] = "パスワードが入力されていません。";
-                     }else{
-                         $passward_hide = str_repeat('*',strlen($passward)); 
-                     }
-                     if($username === ""){
+                 if($username == "" || $password == "" || $confirm_password == ""){
+                     if($username == ""){
                          $errors['username'] = "ユーザー名が入力されていません。";
                      }
+                     if(($password === "") || ($confirm_password === "")){
+                         if($password === ""){
+                             $errors['password'] = "パスワードが入力されていません。";
+                         }
+                         if($confirm_password === ""){
+                             $errors['confirm_password'] = "パスワード（確認）が入力されていません。";
+                         }
+                     }
                  }else{
-                     $errors = array();
+                     if(!empty($errors['username'])){
+                         unset($errors['username']);
+                     }
+                     if(!empty($errors['password'])){
+                         unset($errors['password']);
+                     }
+                     if(!empty($errors['confirm_password'])){
+                         unset($errors['confirm_password']);
+                     }
+                 }
+                 if($username != ""){
+                     $_SESSION['username'] = $username;
+                 }
+                 if(count($errors) === 0 && ($password != "") && ($confirm_password != "")){
+                     if($password == $confirm_password){
+                         $password_hide = str_repeat('*',strlen($password));
+                         $_SESSION['password'] = $password;
+                         $_SESSION['password_hide'] = $password_hide;
+                     }else{
+                             $errors['password_confirm'] = "パスワードが一致しません。";
+                     }
                  }
              }
          }
@@ -104,16 +135,18 @@
          if(isset($_POST['btn_submit'])){
              //パスワードのハッシュ化
              $password_hash = password_hash($_SESSION['password'], PASSWORD_DEFAULT);
-         
+             $email = $_SESSION['email'];
+             $username = $_SESSION['username'];
+             
             //ここでデータベースに登録する
             try{
                  $newdate = date('Y-m-d H:i:s');
                  $newstatus = 1;
-                 $sql_registerate = "INSERT INTO user (username, password, status, createddate, updateddate)";
+                 $sql_registerate = "INSERT INTO user (email, username, password, status, createddate, updateddate) VALUES(:email, :username, :password, :status, :createddate, :updateddate)";
                  $stmt_registerate = $pdo -> prepare($sql_registerate);
-                 $stmt_registerate -> bindValue(':email', $_SESSION['email'], PDO::PARAM_STR);
-                 $stmt_registerate -> bindValue(':username', $_SESSION['username'], PDO::PARAM_STR);
-                 $stmt_registerate -> bindValue(':password', $password_harsh, PDO::PARAM_STR);
+                 $stmt_registerate -> bindValue(':email', $email, PDO::PARAM_STR);
+                 $stmt_registerate -> bindValue(':username', $username, PDO::PARAM_STR);
+                 $stmt_registerate -> bindValue(':password', $password_hash, PDO::PARAM_STR);
                  $stmt_registerate -> bindValue(':status', $newstatus, PDO::PARAM_INT);
                  $stmt_registerate -> bindValue(':createddate', $newdate, PDO::PARAM_STR);
                  $stmt_registerate -> bindValue(':updateddate', $newdate, PDO::PARAM_STR);
@@ -153,22 +186,22 @@
    <h1>ようこそ、本登録をお願いします。</h1>
    
    <!-- page3 完了画面 -->
-   <?php if(isset($_POST['btn_submit']) && count($errors) === 0): ?>
+   <?php if(isset($_POST['btn_submit']) && count($errors) == 0): ?>
    　本登録されました。
    　<p>ログインは<a href="m6-login-form.php">こちら</a></p>
    　
    <!-- page2 確認画面 -->
-   <?php elseif(isset($_POST['btn_confirm']) && count($errors) === 0): ?>
+   <?php elseif(isset($_POST['btn_confirm']) && count($errors) == 0): ?>
          <form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>?urltoken=<?php print $urltoken; ?>" method="post">
              <p>メールアドレス: <?=htmlspecialchars($_SESSION['email'], ENT_QUOTES)?></p>
-             <p>ユーザー名: <?=htmlspecialchars($username, ENT_QUOTES)?></p>
-             <p>パスワード: <?=$password_hide?></p>
+             <p>ユーザー名: <?=  $_SESSION['username'] ?></p>
+             <p>パスワード: <?=$_SESSION['password_hide']?></p>
              
              <input type="submit" name="btn_back" value="戻る">
              <input type="hidden" name="token" value="<?=$_POST['token']?>">
              <input type="submit" name="btn_submit" value="登録する">
          </form>
-   <?php endif; ?>
+   
    
      <!-- page1 登録画面 -->
          <?php if(count($errors) > 0):?>
@@ -178,15 +211,15 @@
              }
              ?>
          <?php endif; ?>
-             <?php if(!isset($errors['urltoken_timeover'])): ?>
+    <?php elseif(!isset($errors['urltoken_timeover'])): ?>
                 <form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>?urltoken=<?php print $urltoken; ?>" method="post">
 				   <p>メールアドレス：    <?=htmlspecialchars($email, ENT_QUOTES, 'UTF-8')?></p>
-				   <p>ユーザー名：        <input type="text" name="name" value="<?php if( !empty($_SESSION['name']) ){ echo $_SESSION['name']; } ?>"></p>
+				   <p>ユーザー名：        <input type="text" name="username" value="<?php if( !empty($_SESSION['username']) ){ echo $_SESSION['username']; } ?>"></p>
 				   <p>パスワード：        <input type="password" name="password"></p>
 				   <p>パスワード（確認）：<input type="password" name="confirm_password"></p>
 	   			   <input type="hidden" name="token" value="<?=$token?>">
 				   <input type="submit" name="btn_confirm" value="確認する">
 	            </form>
-	         <?php endif; ?>
+	<?php endif; ?>
 </body>
 </html>
